4ef913d6f6bfac57c74d7f11bb0575817b3e2b4b
selinkarabel <selinkarabel@gmail.com>
2021-03-03 22:02:27 +0300

    03.03.2021 par1

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,7 @@
 using Business.Abstract;
 using Business.Concrete;
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
@@ -42,8 +44,6 @@ namespace WebAPI
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
             //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); 
-            ServiceTool.Create(services);
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -62,7 +62,10 @@ namespace WebAPI
                     };
                 });
 
-     
+            services.AddDependencyResolvers(new ICoreModule[] {
+                new CoreModule()
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
