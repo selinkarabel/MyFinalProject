807e652d3d209ee1ec01558c7b46ac00dd807e6b
selinkarabel <selinkarabel@gmail.com>
2021-03-11 00:05:58 +0300

    end of 11.03.2021

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -30,8 +31,10 @@ namespace WebAPI.Controllers
         public IActionResult GetAll()
         {
             //Swagger
-           //Dependency chain
-            
+            //Dependency chain
+
+            Thread.Sleep(5000);
+
             var result = _productService.GetAll();
             if(result.Success)
             {
